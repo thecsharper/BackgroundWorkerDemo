43df097caab40689341fab67bf1b2063d4107962
thecsharper <si@thecsharper.com>
2023-05-29 09:49:11 +0100

    Add project files.

---
+++ BackgroundWorkerDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundWorkerDemo
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+        
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Background worker running at: {time}", DateTimeOffset.Now);
+
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+    }
+}
